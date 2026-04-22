21854fc3a138cc1594688e3e236ffdd773f848c2
shya <dan119@bk.ru>
2026-04-22 20:45:29 +0300

    перед созданием CMake

--- main.c
+++ main.c
@@ -5,12 +5,17 @@
 
 int main(){
     FILE *file = fopen("test.bin", "rb");
-    uint8_t buffer_for_read[4];
-    buffer_for_read[3] = '\n';
-    size_t length;
-    while((length = fread(buffer_for_read, sizeof(buffer_for_read)-1, 1, file))!=0){
-        write(1, buffer_for_read, sizeof(buffer_for_read));
-        //putchar('\n');
+    uint32_t size;
+    fread(&size, sizeof(uint32_t), 1, file);
+    //size /= 8;
+    uint8_t buffer[size];
+    //printf("%d\n", size);
+    size_t size_for_reading;
+    while ((size_for_reading = fread(buffer, size, 1, file))!=0) {
+        for(size_t i = 0; i < size; i++){
+            //printf("%d ", buffer[i]);
+        }
+        putchar('\n');
     }
     fclose(file);
     return 0;
