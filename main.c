2a4073ff5a45f092eb79da90adf9e001540fcdab
shya <dan119@bk.ru>
2026-04-25 20:03:52 +0300

    вроде будет работать

--- main.c
+++ main.c
@@ -15,33 +15,36 @@ void print_binary(uint8_t n) {
     putchar((n & 1) ? '1' : '0');
 }
 
-uint8_t write_pixels(array_of_pixels array, char* name_of_file){
-    uint8_t number_to_load = 0;
-    for (size_t j = 0; j < 8; j++) {
-        //printf("%d ", array.array[i*8 + j]);
-        printf("%d", array.array[j] ? 1 : 0);
-        
-    }
-    printf("\n\n");
-    print_binary(number_to_load);
-    return 0;
-}
-
 uint8_t calculate_from_8boolean_values_to_one_bite(uint8_t array[]){
     uint8_t one_bite_value = 0;
     uint8_t power_of_two = 1;
-    for(uint8_t i = 7; i >= 0; i--){
+    for(uint8_t i = 7; i > 0; i--){
         one_bite_value += power_of_two * (array[i] ? 1 : 0);
         power_of_two *= 2;
     }
+    one_bite_value += power_of_two * (array[0] ? 1 : 0);
     return one_bite_value;
+}
+
+uint8_t write_pixels(array_of_pixels array, char* name_of_file){
+    //uint8_t number_to_load = 0;
+    for (size_t i = 0; i < array.length; i+=8) {
+        for (uint8_t j = 0; j < 8; j++) {
+            printf("%d ", array.array[i+j]);
+        }
+        printf("\n%b\n", calculate_from_8boolean_values_to_one_bite(array.array + i));
+    }
+    //printf("\n\n");
+    //print_binary(number_to_load);
+    return 0;
 }
 
 int main(){
     array_of_pixels array;
     uint8_t array2[] = {0, 255, 255, 255, 0, 255, 0, 255, 0, 255, 0, 255, 0, 255, 0, 255};
-    array.array = array2;
+    printf("%b\n", calculate_from_8boolean_values_to_one_bite(array2));
+    /*array.array = array2;
     array.length = sizeof(array2)/8;
-    write_pixels(array, "test.obi");
+    write_pixels(array, "test.obi");*/
     return 0;
 }
